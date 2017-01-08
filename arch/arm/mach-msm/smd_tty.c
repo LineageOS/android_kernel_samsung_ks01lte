cbc72df0a9a1cdcf934fb968b86d8aee9ffa4414
Vitalii Kulikov <solk@solk.org.ua>
2017-01-08 12:44:32 +0100

    smd: Bring back legacy_ds
    Change-Id: Ic904c5297dfb47dd91ad6bbbe665772bef43a143

--- arch/arm/mach-msm/smd_tty.c
+++ arch/arm/mach-msm/smd_tty.c
@@ -858,6 +858,9 @@ static int smd_tty_core_init(void)
 			legacy_ds |= cpu_is_msm8x60() &&
 					(socinfo_get_platform_subtype() == 0x0);
 
+			/* Samsung request for all project */
+			legacy_ds = 1;
+
 			if (!legacy_ds)
 				continue;
 		}
